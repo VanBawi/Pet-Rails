e210cfd1fa3e1a73ae83656c1043a3d88e777217
Jack <superjackpower85@gmail.com>
2019-08-01 00:44:58 +0800

    facebookauth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,19 @@
 class UsersController < ApplicationController
     
-    before_action :authenticate_user!
+   #  before_action :authenticate_user!
 
-    user_signed_in?
+   #  user_signed_in?
 
-    current_user
+   #  current_user
 
-    user_session
+   #  user_session
 
-    def is_signed_in?
-        if !user_signed_in?
-           redirect_to new_user_session_path
-        else 
-           redirect_to "post#index"
-        end
-     end
+   #  def is_signed_in?
+   #      if !user_signed_in?
+   #         redirect_to new_user_session_path
+   #      else 
+   #         redirect_to "post#index"
+   #      end
+   #   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
 
 
   def self.new_with_session(params, session)
@@ -17,8 +17,8 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
+      # user.name = auth.info.name   # assuming the user model has a name
+      # user.image = auth.info.image # assuming the user model has an image
     end
   end
 end
