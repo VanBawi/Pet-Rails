89b3ea1fffbf151732e61ffa664080fcea43e306
Jack <superjackpower85@gmail.com>
2019-08-05 01:39:58 +0800

    nilclassfixe1d

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
     
-    before_action :authenticate_user!
+    # before_action :authenticate_user!
 
-    user_signed_in?
+    # user_signed_in?
 
-    current_user
+    # current_user
 
-    user_session
+    # user_session
 
    #  def is_signed_in?
    #      if !user_signed_in?
