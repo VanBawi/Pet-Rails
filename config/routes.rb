c20b974a65f14611f8a92fedcdff9a7ed8f14f0d
Jack <superjackpower85@gmail.com>
2019-08-06 11:29:56 +0800

    gmail_api added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  resources :profiles
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' } 
+
   root to: 'welcome#show'
 
   resources :pets
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_02_084040) do
+ActiveRecord::Schema.define(version: 2019_08_06_012656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,14 @@ ActiveRecord::Schema.define(version: 2019_08_02_084040) do
     t.string "title"
     t.text "description"
     t.string "picture"
+    t.bigint "user_id"
     t.string "images"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_pets_on_user_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -64,7 +71,6 @@ ActiveRecord::Schema.define(version: 2019_08_02_084040) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -81,5 +87,6 @@ ActiveRecord::Schema.define(version: 2019_08_02_084040) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "pets", "users"
   add_foreign_key "rents", "users"
 end
