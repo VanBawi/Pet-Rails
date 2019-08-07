9a0d3fe5a1ad33a7eca2bf71ab0a50615190e7f7
Jack <superjackpower85@gmail.com>
2019-08-07 11:07:32 +0800

    added -c- 5n commented

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
 
-  def send_devise_notification(notification, *args)
-    devise_mailer.send(notification, self, *args).deliver_later
-  end
+  # def send_devise_notification(notification, *args)
+  #   devise_mailer.send(notification, self, *args).deliver_later
+  # end
 
   def self.new_with_session(params, session)
     super.tap do |user|
