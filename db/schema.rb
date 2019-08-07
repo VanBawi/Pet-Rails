5dbc9e0bf2b47758ae4252ab0d5e16ad42a5fbe5
Jack <superjackpower85@gmail.com>
2019-08-07 08:18:21 +0800

    css added

--- db/schema.rb
+++ db/schema.rb
@@ -40,11 +40,9 @@ ActiveRecord::Schema.define(version: 2019_08_06_012656) do
     t.string "title"
     t.text "description"
     t.string "picture"
-    t.bigint "user_id"
     t.string "images"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -71,6 +69,7 @@ ActiveRecord::Schema.define(version: 2019_08_06_012656) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -87,6 +86,5 @@ ActiveRecord::Schema.define(version: 2019_08_06_012656) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "pets", "users"
   add_foreign_key "rents", "users"
 end
