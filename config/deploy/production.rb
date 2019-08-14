04216e1c94e568eece05842b27d582054fd2dac7
Jack <superjackpower85@gmail.com>
2019-08-14 12:48:22 +0800

    added app web in config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,8 +5,7 @@
 
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
- server "3.0.99.78", user: "deploy", roles: %w{db}
-
+ server "3.0.99.78", user: "deploy", roles: %w{app db web}
 
 
 # role-based syntax
